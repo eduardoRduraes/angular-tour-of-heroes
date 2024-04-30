8f58e820abd5a280b3271104132295e26c5b3340
Eduardo R. Duraes <eduardoduraes.bsi@gmail.com>
2024-04-29 21:52:36 -0300

    The hero editor

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Hero} from "../hero";
 
 @Component({
   selector: 'app-heroes',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  hero = "WildStorm";
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 }
